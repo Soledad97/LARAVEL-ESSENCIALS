adb402cc3a6e765fb0b3d6fe55d60007ccb27fb4
mario <mariones158@gmail.com>
2020-01-02 20:36:04 -0300

    hicimos el fillable

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['']
+    protected $fillable = ['category'];
+
+    public function products (){
+
+    return $this->hasMany(App/Product);
+
+    }
 }
