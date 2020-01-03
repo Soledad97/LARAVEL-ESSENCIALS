daa4443720f14662e09534a50127c43678702677
mario <mariones158@gmail.com>
2020-01-03 18:25:55 -0300

    category de controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('website.Categories.index', ['categories' => Category::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $category = Category::create($request->all());
+
+        return redirect('/categories/' . $category->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show($category)
+    {
+        return view('website.categories.show', ['category' => Category::findOrFail($category)]);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($category)
+    {
+        return view ('admin.categories.edit', ['category' => Category::findOrFail($category)]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $category)
+    {
+        $category = Category::find($category);
+        $category->update($request->all());
+        return redirect('/categories/' . $category->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($category)
+    {
+        $category = Category::findOrFail($category);
+
+        $category->delete();
+
+        return redirect('/categories');
+    }
+}
