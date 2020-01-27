48321bdf36d28fe5a36b5f2ea2632cbd15d04106
mario <mariones158@gmail.com>
2020-01-27 20:48:13 -0300

    rutas del website

---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Crear producto</h1>
+
+                @include('admin.products.form', [
+                    'method' => 'post',
+                    'url' => '/products',
+                ])
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,95 @@
 | contains the "web" middleware group. Now create something great!
 |
 
-*/
+*/ 
+// website.index.blade.php muestra el home de la pagina
+Route::get('/', 'WebsiteController@index');
+
+// TODOS 
+// website.product.index.blade.php muestra un listado de productos
+Route::get('/products', 'ProductController@index');
+
+// website.product.show.blade.php  muestra el detalle de un producto
+Route::get('/products/{id}', 'ProductController@show');
+
+
+
+
+
+
+
+
+// website.purchases.index.blade.php muestra un listado de mis compras
+Route::get('/purchases', 'OrderController@index');
+
+// website.purchases.show.blade.php  muestra el detalle de una compra
+Route::get('/purchases/{id}', 'OrderController@show');
+
+// admin.purchases.create.blade.php  muestra el formulario para finalizar la compra
+Route::get('/purchases/add', 'OrderController@create');
+
+
+Route::group(['middleware'=>'cart'], function(){
+  // website.cart.show.blade.php  muestra el detalle de una compra
+  Route::get('/cart', 'CartController@index');
+
+  // website.cart.create.blade.php muestra la creacion de un carrito
+  Route::post('/cart/add', 'CartController@store');
+
+
+});
+
+
+// RUTAS DE  PRODUCTOS FOTOS // Esto no va //
+
+
+// website.index.blade.php muestra el home de la pagina
+//Route::get('/', 'WebsiteController@index');
+
+// website.product.index.blade.php muestra un listado de productos
+Route::get('/photos', 'PhotoController@index');
+
+// website.product.show.blade.php  muestra el detalle de un producto
+Route::get('/photos/{id}', 'PhotoController@show');
+
+// admin.product.create.blade.php  muestra el formulario para crear un producto
+Route::get('/admin/photos/add', 'PhotoController@create');
+
+// admin.product.edit.blade.php  muestra el formulario para editar un producto
+Route::get('/admin/photos/{id}/edit', 'PhotoController@edit');
+
+
+
+// admin.customer.show.blade.php  muestra el formulario para finalizar la compra
+Route::get('/profile', 'UserController@show');
+
+Route::post('/profile', 'UserController@show');
+
+
+
+
+Route::get('/categories', 'CategoryController@index');
+
+Route::get('/categories/{id}', 'CategoryController@show');
+
+Route::get('/admin/categories/add', 'CategoryController@create');
+
+Route::get('/admin/categories/{id}/edit', 'CategoryController@create');
+
+
+
+Route::get('/payments', 'paymentsController@index');
+
+Route::get('/payments/{id}', 'paymentsController@show');
+
+Route::get('/admin/payments/add', 'paymentsController@create');
+
+Route::get('/admin/payments/{id}/edit', 'paymentsController@create');
+ 
+
+
+
+
 Route::group(['prefix'=>'admin', 'middlerware'=>[]], function(){
   // tablas : views admin.index.blade.php
   Route::get('address', 'AddressController@index');
