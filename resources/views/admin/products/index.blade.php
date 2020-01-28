6117731908d6d6133a2329bb514770072dc99b78
mario <mariones158@gmail.com>
2020-01-28 15:59:01 -0300

    index.blade

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,5 +1,26 @@
-@extends('layouts.admin')
+@extends('layouts.admin.products')
 
 @section('content')
-@endsection
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>{{ $title }}</h1>
 
+                @include('admin.product.index', [
+                    'method' => 'get',
+                    'url' => '/products/' . $product->id
+                ])
+
+                @foreach ($products as $product)
+                    <div>
+                        <a href="/products/{{ $product->id }}">
+                            {{ $product->name }}
+                        </a>
+                    </div>
+                @endforeach
+
+
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/website/products/index.blade.php
+++ resources/views/website/products/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts')
+@extends('layouts.website.products')
 
 
 @section('title')
